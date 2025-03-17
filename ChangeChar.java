93e09de680de947ac2567601336aa08ddefce7d5
Stan.Fang <113745863+StanFCN@users.noreply.github.com>
2025-03-17 17:32:15 +0800

    Initial commit

---
+++ ChangeChar.java
@@ -0,0 +1,14 @@
+/*
+演示转义字符
+\t占位符
+\\一个\
+\"一个"
+\r回车(回车加ins)
+\n换行
+*/
+public class ChangeChar{
+	public static void main(String[] args)
+	{
+		System.out.println("书名\t作者\t价格\t销量\t\n三国\t罗贯中\t120\t1000\t");
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+	public static void main(String args[]){
+		System.out.println("小方 is studying java!");
+	}
+}
+//这是一个java简单程序
