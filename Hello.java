8fea4868cbd8c8bcded69d58d03b85ea0c5e5ae4
Stan.Fang <113745863+StanFCN@users.noreply.github.com>
2025-03-18 08:41:29 +0800

    Update Hello.java
    写了一个小狗类

--- Hello.java
+++ Hello.java
@@ -3,4 +3,11 @@ public class Hello{
 		System.out.println("小方 is studying java!");
 	}
 }
+
+class Dog{
+	public static void main(String args[])
+	{
+		System.out.println("小狗说:\"我是匀迈伞的走狗!\"");
+	}
+}
 //这是一个java简单程序
